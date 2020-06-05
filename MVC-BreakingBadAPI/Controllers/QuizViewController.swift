abb10ff7ae5bf2358d6e78f93e77a5acc2f0803e
aleksiosdev <aleksios@manychat.com>
2020-06-05 20:00:23 +0300

    FUCKING AMAZING QUIZ

--- MVC-BreakingBadAPI/Controllers/QuizViewController.swift
+++ MVC-BreakingBadAPI/Controllers/QuizViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 enum State {
 	case initial
-	case quizStep(step: QuizStep)
+	case quiz
 	case finished
 }
 
@@ -21,20 +21,34 @@ struct QuizStep {
 }
 
 class StepsModel {
-	private let _quizSteps: [QuizStep] = []
-
+	private let _quizSteps: [QuizStep] = [
+		QuizStep(question: "Who killed Walter White",
+				 variants: ["Mike Ehrmantraut", "Christian Ortgea", "Skinny Pete", "Unknown"],
+				 correctVariantIndex: 1) ]
 	private var _currentStepIndex: Int = 0
-
 	private var _correctAnswers: Int = 0
+	private var _state: State = .initial
 
 	func checkIfStepIsCorrect(index: Int) -> Bool {
 		return _quizSteps[_currentStepIndex].correctVariantIndex == index
 	}
 
-	func nextStep() -> State {
-		_currentStepIndex += 1
-		guard _currentStepIndex < _quizSteps.count else { return .finished }
-		return .quizStep(step: _quizSteps[_currentStepIndex])
+	func nextStep() -> QuizStep? {
+		switch _state {
+		case .initial:
+			_currentStepIndex = 0
+			_state = .quiz
+		case .quiz:
+			_currentStepIndex += 1
+			guard _currentStepIndex < _quizSteps.count else {
+				_state = .finished
+				return nil
+			}
+		case .finished:
+			return nil
+		}
+
+		return _quizSteps[_currentStepIndex]
 	}
 }
 
@@ -75,15 +89,7 @@ class QuizView: UIView {
 }
 
 final class QuizViewController: UIViewController {
-
-	func showStartState() {
-
-	}
-
-	func showQuiz() {
-
-	}
-
+	let model = StepsModel()
 
     override func loadView() {
         let quizView = QuizView()
@@ -93,13 +99,19 @@ final class QuizViewController: UIViewController {
 
     @objc private func onStart() {
         let stepView = StepView()
+		guard let quizStep = model.nextStep() else { return }
+		stepView.titleLabel.text = quizStep.question
+		stepView.answerTwoButton.setTitle(quizStep.variants.first, for: .normal)
+		stepView.answerFirstbutton.setTitle(quizStep.variants[1], for: .normal)
         self.view = stepView
+
     }
 
     class StepView : UIView {
         lazy var mainStackView: UIStackView = {
             let stackView = UIStackView()
-            stackView.axis = .horizontal
+			stackView.spacing = 16
+            stackView.axis = .vertical
             stackView.translatesAutoresizingMaskIntoConstraints = false
             return stackView
         }()
@@ -113,24 +125,43 @@ final class QuizViewController: UIViewController {
 
         lazy var answersStackView: UIStackView = {
             let stackView = UIStackView()
-            stackView.axis = .vertical
+            stackView.axis = .horizontal
             stackView.translatesAutoresizingMaskIntoConstraints = false
             return stackView
         }()
 
         lazy var titleLabel: UILabel = {
             let label = UILabel()
-//            label.text = "Win a quiz game or get a spoiler!"
+			label.font = UIFont.boldSystemFont(ofSize: 24)
             label.textColor = .white
+			label = .center
             return label
         }()
+
+		lazy var answerFirstbutton: UIButton = {
+			let button = UIButton()
+			button.translatesAutoresizingMaskIntoConstraints = false
+			return button
+		}()
+
+		lazy var answerTwoButton: UIButton = {
+			let button = UIButton()
+			button.translatesAutoresizingMaskIntoConstraints = false
+			return button
+		}()
+
+
 
         override init(frame: CGRect) {
             super.init(frame: frame)
             self.addSubview(mainStackView)
-            self.mainStackView.addSubview(self.scoreStackView)
-            self.mainStackView.addSubview(self.titleLabel)
-            self.mainStackView.addSubview(self.answersStackView)
+            self.mainStackView.addArrangedSubview(self.scoreStackView)
+            self.mainStackView.addArrangedSubview(self.titleLabel)
+            self.mainStackView.addArrangedSubview(self.answersStackView)
+			self.answersStackView.addArrangedSubview(answerFirstbutton)
+			self.answersStackView.addArrangedSubview(answerTwoButton)
+			mainStackView.leftAnchor.constraint(equalTo: self.leftAnchor).isActive = true
+			mainStackView.rightAnchor.constraint(equalTo: self.rightAnchor).isActive = true
             mainStackView.centerXAnchor.constraint(equalTo: self.centerXAnchor).isActive = true
             mainStackView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
         }
